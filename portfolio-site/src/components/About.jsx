fcf0d1d2a51bb22390203091bf79c593b96d8cb2
Nyene-Abasi <nyeneabasi3@gmail.com>
2023-07-30 00:12:38 +0100

    resume added

--- portfolio-site/src/components/About.jsx
+++ portfolio-site/src/components/About.jsx
@@ -5,7 +5,7 @@ const About = () => {
     <div id='about' className='about'>
         <h1 className='head'>Hi! I'm Nyene</h1>
         <p className='desc'>You're welcome to my portfolio site! I'm a passionate developer with a love for design, technology, and problem-solving. Explore my diverse projects, showcasing creativity and functionality. Let's connect and collaborate on exciting opportunities. Thank you for visiting!</p>
-        <button className='resume'>Resume</button>
+       <a href="https://drive.google.com/file/d/1kqAvNZynQDt6o3FtJ8k-DgA0m4WRK0wD/view"> <button className='resume'>Resume</button> </a>
     </div>
   )
 }
